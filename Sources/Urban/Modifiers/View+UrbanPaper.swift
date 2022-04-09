2638e4dbc2502e2fed2a2204007b121308ceb242
Kris Torres <torres-kris@outlook.com>
2022-04-09 02:57:05 -0700

    Implement the urbanPaper view modifier

---
+++ Sources/Urban/Modifiers/View+UrbanPaper.swift
@@ -0,0 +1,69 @@
+import SwiftUI
+
+public extension View {
+    
+    /// Displays the view on a paperlike surface.
+    ///
+    /// - Parameter hasBorder: Indicates whether the paper element has a border.
+    ///                        The default is `false`.
+    /// - Parameter hasShadow: Indicates whether the paper element has a shadow.
+    ///                        The default is `true`.
+    ///
+    /// - Returns: The modified view.
+    func urbanPaper(
+        border hasBorder: Bool = false,
+        shadow hasShadow: Bool = true
+    ) -> some View {
+        return modifier(UrbanPaper(hasBorder: hasBorder, hasShadow: hasShadow))
+    }
+}
+
+/// A simple view modifier that displays content on a paperlike surface.
+fileprivate struct UrbanPaper: ViewModifier {
+    
+    /// Indicates whether the paper element has a border.
+    let hasBorder: Bool
+    
+    /// Indicates whether the paper element has a shadow.
+    let hasShadow: Bool
+    
+    /// The Urban theme.
+    @Environment(\.urbanTheme) private var theme
+    
+    /// Returns the current body of the caller.
+    ///
+    /// - Parameter content: The content view.
+    ///
+    /// - Returns: The caller’s body.
+    func body(content: Content) -> some View {
+        return content
+            .foregroundColor(theme.palette.surface.content)
+            .background(theme.palette.surface.main)
+            .clipShape(shape)
+            .overlay(borderLayer)
+            .shadow(radius: hasShadow ? Constants.shadowRadius : 0)
+    }
+    
+    /// The border “layer” of the paper element.
+    private var borderLayer: some View {
+        Group {
+            if hasBorder {
+                shape.stroke(
+                    theme.palette.surface.content,
+                    lineWidth: Constants.borderWidth
+                )
+            }
+        }
+    }
+    
+    /// The shape of the paper element.
+    private var shape: some Shape {
+        RoundedRectangle(cornerRadius: theme.cornerRadius)
+    }
+    
+    /// An internal enum that contains drawing constants.
+    private enum Constants {
+        static let borderWidth: CGFloat = 2
+        static let shadowRadius: CGFloat = 4
+    }
+}
